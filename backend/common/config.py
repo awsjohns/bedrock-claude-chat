8f6b0b094603a72d97fa91a362ec82df46f23222
John Stephens <awsjohns@amazon.com>
2023-09-21 20:32:31 +0000

    changing inference config

--- backend/common/config.py
+++ backend/common/config.py
@@ -2,7 +2,8 @@
 # ご利用のアプリケーションに合わせて調整してください。
 # 参考: https://docs.anthropic.com/claude/reference/complete_post
 GENERATION_CONFIG = {
-    "maxTokenCount": 500,
+    "maxTokenCount": 512,
     "temperature": 0,
+    "topP": 0.9,
     "stopSequences": []
 }
